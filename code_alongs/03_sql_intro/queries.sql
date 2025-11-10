ed6f6648f415216ecb81593f3dd2eac17a4f586f
Lucas Lindh <lucas.lindh6@live.se>
2025-11-10 15:35:04 +0100

    Update

--- code_alongs/03_sql_intro/queries.sql
+++ code_alongs/03_sql_intro/queries.sql
@@ -1,1 +1,18 @@
 SELECT * FROM funny_jokes;
+
+-- sort by rating
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER by
+    rating;
+
+-- sort by rating BY descending order
+SELECT
+    *
+FROM
+    funny_jokes
+ORDER by
+    rating
+DESC;

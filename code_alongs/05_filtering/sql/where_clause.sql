6da89b8d8031b73314374ee4c1536525dd7ae2f0
Lucas Lindh <lucas.lindh6@live.se>
2025-11-11 19:54:34 +0100

    video along 05

---
+++ code_alongs/05_filtering/sql/where_clause.sql
@@ -0,0 +1,42 @@
+SELECT
+    COUNT(*)
+FROM
+    data_jobs;
+
+SELECT
+    COUNT(*)
+FROM
+    data_jobs
+WHERE
+    salary_in_usd < 50000;
+
+-- find entry level jobs
+SELECT DISTINCT
+    experience_level
+FROM
+    data_jobs;
+
+SELECT
+    *
+FROM
+    data_jobs
+WHERE
+    experience_level = 'EN';
+
+-- find median salary for entry level jobs
+SELECT
+    MEDIAN(salary_in_usd) AS median_salary_usd,
+    AVG(salary_in_usd) AS mean_salary_usd
+FROM
+    data_jobs
+WHERE
+    experience_level = 'EN';
+
+-- find median salary for mid level jobs
+SELECT
+    MEDIAN(salary_in_usd) AS median_salary_usd,
+    AVG(salary_in_usd) AS mean_salary_usd
+FROM
+    data_jobs
+WHERE
+    experience_level = 'MI';

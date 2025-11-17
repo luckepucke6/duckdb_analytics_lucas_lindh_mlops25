4e507952df563ad824cdcb4366203b48302e0881
Lucas Lindh <lucas.lindh6@live.se>
2025-11-17 10:46:11 +0100

    crud

--- code_alongs/06_crud_operations/sql/create_statement.sql
+++ code_alongs/06_crud_operations/sql/create_statement.sql
@@ -1,34 +1,46 @@
-CREATE SCHEMA IF NOT EXISTS database;
+CREATE SCHEMA IF NOT EXISTS database_schema;
 
-CREATE SCHEMA IF NOT EXISTS programming;
+CREATE SCHEMA IF NOT EXISTS programming_schema;
 
 -- check schemas
-FROM information_schema.schemata;
+FROM
+    information_schema.schemata;
 
+SELECT
+    *
+FROM
+    information_schema.schemata
+WHERE
+    catalog_name = 'glossary';
 
 -- sequences
 CREATE SEQUENCE IF NOT EXISTS id_sql_sequence START 1;
-CREATE SEQUENCE IF NOT EXISTS id_python_sequence START 1;
+
 CREATE SEQUENCE IF NOT EXISTS id_duckdb_sequence START 1;
 
+CREATE SEQUENCE IF NOT EXISTS id_python_sequence START 1;
 
-FROM pg_catalog.pg_sequences;
+FROM
+    pg_catalog.pg_sequences;
 
 -- creating glossary tables
-CREATE TABLE IF NOT EXISTS database.sql (
-    id INTEGER PRIMARY KEY DEFAULT nextval('id_sql_sequence'),
-    word STRING,
-    description STRING
-);
+CREATE TABLE
+    IF NOT EXISTS database_schema.sql_table (
+        id INTEGER PRIMARY KEY DEFAULT nextval ('id_sql_sequence'),
+        word STRING,
+        description STRING
+    );
 
-CREATE TABLE IF NOT EXISTS database.duckdb (
-    id INTEGER PRIMARY KEY DEFAULT nextval('id_duckdb_sequence'),
-    word STRING,
-    description STRING
-);
+CREATE TABLE
+    IF NOT EXISTS database_schema.duckdb_table (
+        id INTEGER PRIMARY KEY DEFAULT nextval ('id_duckdb_sequence'),
+        word STRING,
+        description STRING
+    );
 
-CREATE TABLE IF NOT EXISTS programming.pyhton (
-    id INTEGER PRIMARY KEY DEFAULT nextval('id_python_sequence'),
-    word STRING,
-    description STRING
-);
+CREATE TABLE
+    IF NOT EXISTS programming.pyhton (
+        id INTEGER PRIMARY KEY DEFAULT nextval ('id_python_sequence'),
+        word STRING,
+        description STRING
+    );

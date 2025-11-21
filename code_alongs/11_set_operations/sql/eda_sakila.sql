ef18f1bbaa27fa1bf702f9100824e64da8cd80c8
Lucas Lindh <lucas.lindh6@live.se>
2025-11-21 17:15:15 +0100

    lecture 11 coding done

---
+++ code_alongs/11_set_operations/sql/eda_sakila.sql
@@ -0,0 +1,50 @@
+FROM
+    actor;
+
+FROM
+    film;
+
+FROM
+    film_actor;
+
+FROM
+    address;
+
+FROM
+    city;
+
+FROM
+    country;
+
+FROM
+    category;
+
+FROM
+    customer;
+
+SELECT
+    COUNT(*) AS number_movies,
+    COUNT(DISTINCT title) AS unique_number_of_titles
+FROM
+    film;
+
+SELECT DISTINCT
+    rating
+FROM
+    main.film;
+
+DESC TABLE film_actor;
+
+FROM
+    customer;
+
+SELECT
+    'customer' AS TYPE,
+    c.first_name,
+    c.last_name,
+FROM
+    customer c
+WHERE
+    c.first_name LIKE 'D%';
+
+-- more EDAs left for the reader
---
+++ code_alongs/11_set_operations/sql/set_operation_sakila.sql
@@ -0,0 +1,46 @@
+-- selects all customers and all actors starting first name with A
+
+SELECT
+    'customer' AS TYPE,
+    c.first_name,
+    c.last_name,
+FROM
+    customer c
+WHERE
+    c.first_name LIKE 'A%'
+UNION
+SELECT
+    'Actor',
+    a.first_name,
+    a.last_name
+FROM
+    actor a
+WHERE
+    a.first_name LIKE 'A%'
+ORDER BY first_name;
+
+-- find overlapping names between actors and customers
+SELECT
+    a.first_name,
+    a.last_name
+FROM actor a
+INTERSECT
+SELECT
+    c.first_name,
+    c.last_name
+FROM customer c;
+
+-- find all with initials JD and record its type (actor, customer)
+SELECT
+    'actor' AS "type",
+    a.first_name,
+    a.last_name
+FROM actor a
+WHERE a.first_name LIKE 'J%' AND a.last_name LIKE 'D%'
+UNION ALL
+SELECT
+    'customer' AS "type",
+    c.first_name,
+    c.last_name
+FROM customer c 
+WHERE c.first_name LIKE 'J%' AND c.last_name LIKE 'D%';
